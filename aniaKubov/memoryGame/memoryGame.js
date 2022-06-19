e3a6ec175d9982271b66ee3852a1135359665386
danish9811 <danish.mehmood@otif-solutions.com>
2022-06-19 13:50:20 +0500

    written some js lines

--- aniaKubov/memoryGame/memoryGame.js
+++ aniaKubov/memoryGame/memoryGame.js
@@ -1,30 +1,40 @@
-// array of objects
-const cardArray = [
-    {
-        name: 'blank',
-        img: 'images/blank.png'
-    }, {
-        name: 'cheeseburger',
-        img: 'images/cheeseburder.png'
-    }, {
-        name: 'fries',
-        img: 'images/fries.png'
-    }, {
-        name: 'hotdog',
-        img: 'images/hotdog.png'
-    }, {
-        name: 'ice-cream',
-        img: 'images/ice-cream.png'
-    }, {
-        name: 'milkshake',
-        img: 'images/milkshake.png'
-    }, {
-        name: 'pizza',
-        img: 'images/pizza.png'
-    }, {
-        name: 'white',
-        img: 'images/white.png'
-    }
+const cardArray = [{
+    name: 'blank',
+    img: 'images/blank.png'
+}, {
+    name: 'cheeseburger',
+    img: 'images/cheeseburder.png'
+}, {
+    name: 'fries',
+    img: 'images/fries.png'
+}, {
+    name: 'hotdog',
+    img: 'images/hotdog.png'
+}, {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png'
+}, {
+    name: 'milkshake',
+    img: 'images/milkshake.png'
+}, {
+    name: 'pizza',
+    img: 'images/pizza.png'
+}, {
+    name: 'white',
+    img: 'images/white.png'
+}, {
+    name: 'fries',
+    img: 'images/fries.png'
+}, {
+    name: 'white',
+    img: 'images/white.png'
+}, {
+    name: 'hotdog',
+    img: 'images/hotdog.png'
+}, {
+    name: 'milkshake',
+    img: 'images/milkshake.png'
+},
 ]
 
 // shorter way of shuffling the array
@@ -33,12 +43,12 @@ cardArray.sort(() => 0.5 - Math.random())
 const gridDisplay = document.querySelector('#grid')
 
 function createBoard() {
-    for(let i = 1; i <= 10; i++) {
+    for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
         gridDisplay.appendChild(card)
-        console.log(card, i)
+        // console.log(card, i)
     }
 }
 
