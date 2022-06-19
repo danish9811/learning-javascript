68a5c17a61365e7896a92704eac8922cf63d6759
danish9811 <danish.mehmood@otif-solutions.com>
2022-06-19 08:54:00 +0500

    added more logic for the game

--- aniaKubov/rockPaperScissor/mainRock.js
+++ aniaKubov/rockPaperScissor/mainRock.js
@@ -1,15 +1,15 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
-
 const possibleChoices = document.querySelectorAll('button')
 
-let userChoice, computerChoice
+let userChoice, computerChoice, result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 
@@ -28,4 +28,37 @@ function generateComputerChoice() {
 
     computerChoiceDisplay.innerHTML = computerChoice
 
+}
+
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'its a draw!'
+    }
+
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'You win!'
+    }
+
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = 'You lost'
+    }
+
+    if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'You win'
+    }
+
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'You lost'
+    }
+
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'You win'
+    }
+
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'You lost'
+    }
+
+    resultDisplay.innerText = result
 }
