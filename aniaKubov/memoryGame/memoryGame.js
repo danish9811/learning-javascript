cca34f7fd7720b9734191f15cef8c2015d0a7679
danish9811 <danish.mehmood@otif-solutions.com>
2022-06-19 13:38:51 +0500

    added some lines of js

---
+++ aniaKubov/memoryGame/memoryGame.js
@@ -0,0 +1,45 @@
+// array of objects
+const cardArray = [
+    {
+        name: 'blank',
+        img: 'images/blank.png'
+    }, {
+        name: 'cheeseburger',
+        img: 'images/cheeseburder.png'
+    }, {
+        name: 'fries',
+        img: 'images/fries.png'
+    }, {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    }, {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    }, {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    }, {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    }, {
+        name: 'white',
+        img: 'images/white.png'
+    }
+]
+
+// shorter way of shuffling the array
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard() {
+    for(let i = 1; i <= 10; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        gridDisplay.appendChild(card)
+        console.log(card, i)
+    }
+}
+
+createBoard()
