66fda53f960667dfd78a3f3f61a58c7be50768e9
danish9811 <danish.mehmood@otif-solutions.com>
2022-06-19 23:07:47 +0500

    added more lines of js

--- aniaKubov/memoryGame/memoryGame.js
+++ aniaKubov/memoryGame/memoryGame.js
@@ -41,15 +41,27 @@ const cardArray = [{
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+const cardsChosen = []
+
 
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
-        // console.log(card, i)
     }
 }
 
+
+
 createBoard()
+
+function flipCard() {
+    console.log(cardArray)
+    const cardId = this.getAttribute('data-id')
+    console.log(cardArray[cardId].name)
+    console.log('clicked', cardId)
+}
+
